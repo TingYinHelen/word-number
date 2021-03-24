e1595492eed3fae2687f376c6b94f4b9f1ec2057
helen <yinhelen.hlj@qq.com>
2021-03-24 16:04:59 +0800

    fix:修改main

--- test/index.spec.js
+++
@@ -1,4 +0,0 @@
-import { numToWord, wordToNum } from './src/index.js/index.js';
-
-console.log('Two:', wordToNum('Two')); // 2
-console.log('3:', stringiNumber.numToWord(3)); // Three
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'word-number.js',
